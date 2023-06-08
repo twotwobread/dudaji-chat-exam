9c0034959a7a0a486e301acf7285ac1c4fe7495a
twotwobread <dltndud9137@naver.com>
2023-06-08 11:23:13 +0900

    [TEST] pre-commit 동작여부 파악을 위한 테스트 커밋 수행

--- client.py
+++ client.py
@@ -1,29 +1,31 @@
 import socket
-from _thread import start_new_thread
 import sys
+from _thread import start_new_thread
 
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 9999
 if len(sys.argv) > 1:
     NAME = sys.argv[1]
 else:
-    NAME = 'Unknown'
+    NAME = "Unknown"
 
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
-def recv_data(client_socket) :
-    while True :
+
+def recv_data(client_socket):
+    while True:
         data = client_socket.recv(1024)
-        print(repr(data.decode()).replace('"',''))
+        print(repr(data.decode()).replace('"', ""))
+
 
 start_new_thread(recv_data, (client_socket,))
 client_socket.send(NAME.encode())
-print (f'{NAME}님이 접속하였습니다.')
+print(f"{NAME}님이 접속하였습니다.")
 
 while True:
-    message = input('')
-    if message == 'quit':
+    message = input("")
+    if message == "quit":
         break
 
     client_socket.send(message.encode())
--- server.py
+++ server.py
@@ -1,53 +1,57 @@
 import socket
 from _thread import start_new_thread
 
-client_sockets = [] 
+client_sockets = []
 
-def threaded(client_socket, addr, NAME): 
+
+def threaded(client_socket, addr, NAME):
     entering_message = f"{NAME}:{addr}님이 접속하였습니다."
     print(entering_message)
 
     while True:
         try:
-            data = client_socket.recv(1024) #1024byte
+            data = client_socket.recv(1024)  # 1024byte
             if not data:
                 print(f"{NAME}님이 나갔습니다.")
                 break
-            print(f'{NAME} [{addr[0]}:{addr[1]}] {repr(data.decode())}')
-            for client in client_sockets :
-                if client != client_socket :
-                    message = f'{NAME}: {repr(data.decode())}'
+            print(f"{NAME} [{addr[0]}:{addr[1]}] {repr(data.decode())}")
+            for client in client_sockets:
+                if client != client_socket:
+                    message = f"{NAME}: {repr(data.decode())}"
                     client.send(message.encode())
 
         except Exception as e:
+            print(e)
             print(f"{NAME}님이 나갔습니다.")
             break
 
-    if client_socket in client_sockets :
+    if client_socket in client_sockets:
         client_sockets.remove(client_socket)
-        print('Rest Clients : ',len(client_sockets))
+        print("Rest Clients : ", len(client_sockets))
     client_socket.close()
 
-HOST = '127.0.0.1'
+
+HOST = "127.0.0.1"
 PORT = 9999
 
-print('>> Server Start')
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET: IP Version 4, SOCK_STREAM: TCP 패킷 허용. row/"stream"/데이터그램 socket
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #포트 여러번 바인드하면 발생하는 에러 방지
+print(">> Server Start")
+server_socket = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM
+)  # AF_INET: IP Version 4, SOCK_STREAM: TCP 패킷 허용. row/"stream"/데이터그램 socket
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # 포트 여러번 바인드하면 발생하는 에러 방지
 server_socket.bind((HOST, PORT))
-server_socket.listen() # 클라이언트를 기다림. 인수로는 동시 접속할 최대한의 클라이언트 수. 실제 송수신은 accept을 통해서
+server_socket.listen()  # 클라이언트를 기다림. 인수로는 동시 접속할 최대한의 클라이언트 수. 실제 송수신은 accept을 통해서
 
 try:
     while True:
-        print('>> Wait')
+        print(">> Wait")
         client_socket, addr = server_socket.accept()
         NAME = client_socket.recv(1024)
         NAME = repr(NAME.decode())
         client_sockets.append(client_socket)
         start_new_thread(threaded, (client_socket, addr, NAME))
         print("참가자 수 : ", len(client_sockets))
-except Exception as e :
-    print ('에러는? : ',e)
+except Exception as e:
+    print("에러는? : ", e)
 finally:
     server_socket.close()
-
