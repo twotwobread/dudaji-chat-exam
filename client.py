28568fa2d95aa9bed751f3fe0f200f2b50b41241
twotwobread <dltndud9137@naver.com>
2023-06-09 09:14:26 +0900

    [FIX] client 코드를 클래스를 이용하여 수정

--- client.py
+++ client.py
@@ -1,39 +1,56 @@
-import json
 import socket
 import sys
-from _thread import start_new_thread
+import threading
 
-from config.connect_config import BUFFER_SIZE, HOST, PORT, QUIT, UNKONWN
-
-if len(sys.argv) > 1:
-    NAME = sys.argv[1]
-else:
-    NAME = UNKONWN
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+from app.config.connect_config import BLANK, BUFFER_SIZE, HOST, PORT, QUIT, UNKNOWN
+from app.dto.chat_dto import ChatDTO
 
 
-def recv_data(client_socket):
-    while True:
-        received_data = client_socket.recv(BUFFER_SIZE).decode()
-        message = eval(json.loads(received_data))
-        name = message["name"]
-        body = message["body"]
-        print(f"{name} : {body}")
+class Client:
+    def __init__(self, host, port, name):
+        self.host = host
+        self.port = port
+        self.name = name
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((host, port))
+
+    def run(self):
+        send_dto = ChatDTO(self.name, BLANK)
+        self.client_socket.send(send_dto.json_data.encode())
+        print(f"{NAME}님이 접속하였습니다.")
+
+        recv_thread = threading.Thread(target=self.recv_data)
+        send_thread = threading.Thread(target=self.send_data)
+        recv_thread.start()
+        send_thread.start()
+
+        recv_thread.join()
+        send_thread.join()
+        print("Client Closed.")
+
+    def recv_data(self):
+        while True:
+            try:
+                recv_dto = ChatDTO.covertFromByteCode(self.client_socket.recv(BUFFER_SIZE).decode())
+                print(recv_dto)
+            except ConnectionAbortedError:
+                break
+
+    def send_data(self):
+        while True:
+            message = input("")
+            send_dto = ChatDTO(self.name, message)
+
+            self.client_socket.send(send_dto.json_data.encode())
+            if message == QUIT:
+                break
+        self.client_socket.close()
 
 
-start_new_thread(recv_data, (client_socket,))
-client_socket.send(json.dumps({"name": NAME}).encode())
-print(f"{NAME}님이 접속하였습니다.")
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        NAME = sys.argv[1]
+    else:
+        NAME = UNKNOWN
 
-while True:
-    sending_data = {}
-    message = input("")
-    if message == QUIT:
-        break
-
-    sending_data["name"] = NAME
-    sending_data["body"] = message
-    client_socket.send(json.dumps(sending_data).encode())
-client_socket.close()
+    Client(HOST, PORT, NAME).run()
