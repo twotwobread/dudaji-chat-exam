d76505874c227f91099190c90c0f71802b8abcaa
twotwobread <dltndud9137@naver.com>
2023-06-09 12:48:58 +0900

    [FIX] command를 이용하기 위해 server에 command를 처리할 스레드와 자료구조 생성, client socket에서는 command를 추가하는 구문 추가

--- app/client_socket.py
+++ app/client_socket.py
@@ -1,11 +1,11 @@
-from dto.chat_dto import ChatDTO
-
+from command.send_broadcast_command import SendBroadcastCommand
 from config.connect_config import BUFFER_SIZE, QUIT
+from dto.chat_dto import ChatDTO
 
 
 class ClientSocket:
-    def __init__(self, Server, logger, client_socket, client_addr, name):
-        self.Server = Server
+    def __init__(self, server, logger, client_socket, client_addr, name):
+        self.server = server
         self.logger = logger
         self.client_socket = client_socket
         self.client_addr = client_addr
@@ -33,7 +33,9 @@ class ClientSocket:
                 if recv_dto.body == QUIT:
                     self.logger.info("%s님이 나갔습니다.", self.name)
                     break
-                self.Server.broadcast(self.client_socket, recv_dto.json_data.encode())
+                self.server.add_command(
+                    SendBroadcastCommand(self.server, self.client_socket, recv_dto.json_data.encode())
+                )
             except Exception:
                 break
-        self.Server.close_client_socket(self)
+        self.server.close_client_socket(self)
--- app/server.py
+++ app/server.py
@@ -1,10 +1,11 @@
 import socket
 import threading
+from collections import deque
 
 from client_socket import ClientSocket
-from dto.chat_dto import ChatDTO
-
+from command.command import Command
 from config.connect_config import BUFFER_SIZE
+from dto.chat_dto import ChatDTO
 
 
 class Server:
@@ -13,10 +14,14 @@ class Server:
         self.host = host
         self.port = port
         self.client_sockets = []
+        self.client_commands = deque()
+
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen()
+
+        self.thread_lock = threading.Lock()
 
     def close_client_socket(self, client_socket: ClientSocket):
         if client_socket in self.client_sockets:
@@ -24,8 +29,13 @@ class Server:
             self.logger.info("Rest Clients : %s", len(self.client_sockets))
         client_socket.close()
 
+    def add_command(self, command: Command):
+        with self.thread_lock:
+            self.client_commands.append(command)
+
     def run(self):
         self.logger.info(">> Wait")
+        threading.Thread(target=self.process_command).start()
         try:
             while True:
                 client_socket, client_addr = self.server_socket.accept()
@@ -42,6 +52,14 @@ class Server:
             self.server_socket.close()
 
     def broadcast(self, socket, message):
-        for client_socket in self.client_sockets:
-            if client_socket.get_client_socket() != socket:
-                client_socket.send(message)
+        with self.thread_lock:
+            for client_socket in self.client_sockets:
+                if client_socket.get_client_socket() != socket:
+                    client_socket.send(message)
+
+    def process_command(self):
+        while True:
+            while self.client_commands:
+                with self.thread_lock:
+                    command = self.client_commands.popleft()
+                command.execute()
